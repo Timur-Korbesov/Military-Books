506e12a1710da3bf3feb272d985b7a6dd3545776
arthast <arsentakhunov@gmail.com>
2022-05-05 18:47:21 +0300

    ready

--- code/main.py
+++ code/main.py
@@ -1,8 +1,8 @@
 import os
 import sqlite3
 
-from flask import Flask, render_template, make_response, session, request
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask import Flask, render_template, request
+from flask_login import LoginManager, login_user, login_required, logout_user
 from flask_restful import abort
 from werkzeug.utils import redirect, secure_filename
 
